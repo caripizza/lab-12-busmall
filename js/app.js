a01095fea0a5398ab0697b92e5abaf0c2573ca73
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-07 08:23:59 -0800

    tweak image and css source links

--- js/app.js
+++ js/app.js
@@ -30,26 +30,26 @@ let allProducts = [];
 let viewedProducts = [];
 
 // use new Image() constructor https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
-var bag = new Image('bag', './assets/bag.jpg');
-var banana = new Image('banana', './assets/banana.jpg');
-var bathroom = new Image('bathroom', './assets/bathroom.jpg');
-var boots = new Image('boots', './assets/boots.jpg');
-var breakfast = new Image('breakfast', './assets/breakfast.jpg');
-var bubblegum = new Image('bubblegum', './assets/bubblegum.jpg');
-var chair = new Image('chair', './assets/chair.jpg');
-var cthulhu = new Image('cthulhu', './assets/cthulhu.jpg');
-var dogduck = new Image('dog-duck', './assets/dog-duck.jpg');
-var dragon = new Image('dragon', './assets/dragon.jpg');
-var pen = new Image('pen', './assets/pen.jpg');
-var pet_sweep = new Image('pet-sweep', './assets/pet-sweep.jpg');
-var scissors = new Image('scissors', './assets/scissors.jpg');
-var shark = new Image('shark', './assets/shark.jpg');
-var sweep = new Image('sweep', './assets/sweep.png');
-var tauntaun = new Image('tauntaun', './assets/tauntaun.jpg');
-var unicorn = new Image('unicorn', './assets/unicorn.jpg');
-var usb = new Image('usb', './assets/usb.gif');
-var water_can = new Image('water-can', './assets/water-can.jpg');
-var wine_glass = new Image('wine-glass', './assets/wine-glass.jpg');
+var bag = new Image('bag', 'assets/bag.jpg');
+var banana = new Image('banana', 'assets/banana.jpg');
+var bathroom = new Image('bathroom', 'assets/bathroom.jpg');
+var boots = new Image('boots', 'assets/boots.jpg');
+var breakfast = new Image('breakfast', 'assets/breakfast.jpg');
+var bubblegum = new Image('bubblegum', 'assets/bubblegum.jpg');
+var chair = new Image('chair', 'assets/chair.jpg');
+var cthulhu = new Image('cthulhu', 'assets/cthulhu.jpg');
+var dogduck = new Image('dog-duck', 'assets/dog-duck.jpg');
+var dragon = new Image('dragon', 'assets/dragon.jpg');
+var pen = new Image('pen', 'assets/pen.jpg');
+var pet_sweep = new Image('pet-sweep', 'assets/pet-sweep.jpg');
+var scissors = new Image('scissors', 'assets/scissors.jpg');
+var shark = new Image('shark', 'assets/shark.jpg');
+var sweep = new Image('sweep', 'assets/sweep.png');
+var tauntaun = new Image('tauntaun', 'assets/tauntaun.jpg');
+var unicorn = new Image('unicorn', 'assets/unicorn.jpg');
+var usb = new Image('usb', 'assets/usb.gif');
+var water_can = new Image('water-can', 'assets/water-can.jpg');
+var wine_glass = new Image('wine-glass', 'assets/wine-glass.jpg');
 
 function Image(name, path) {
     this.name = name;
