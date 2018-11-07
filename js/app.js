56bc22afae3069fadbced2fd4b0729914f969a21
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-07 07:29:44 -0800

    updated stylesheet, added html content for image area, added js notes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 import productsApi from './products-api.js';
+// import ImageItem from './products-display.js';
 // import surveyApi from './survey-api.js';
 
-// get initial data
+
+// get initial data, all products from products.js
 const products = productsApi.getAll();
-console.log(products);
+// console.log(products);
 
-var product1 = products[Math.floor(Math.random() * products.length)]; // get a random word from products array
-console.log(product1);
 
-// const survey = products.map(product => {
-//     return {
-//         name: product.name,
-//         image: product.image,
-//         views: 0,
-//         clicks: 0
-//     };
-// });
+// this code displays one random name/image product pair
+var randomProduct = products[Math.floor(Math.random() * products.length)];
+console.log(randomProduct);
+
+
+// this code displays entire products array including name, image, views, clicks
+const survey = products.map(product => {
+    return {
+        name: product.name,
+        image: product.image,
+        views: 0,
+        clicks: 0
+    };
+});
+console.log(survey);
--- js/products-display.js
+++ js/products-display.js
@@ -1,8 +1,12 @@
-import html from './html.js';
+// import html from './html.js';
+// import productsApi from './products-api.js';
 
-function displayImages(product) {
-    return html`<li class="images">
-    <img src="assets/${image}">
- </li>`;
-}
+// function makeTemplate(product) {
+//     return html`
+//         <li>
+//             <img src="../assets/bag.png">
+//         </li>
+//     `;
+// }
 
+// use new Image() constructor https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
